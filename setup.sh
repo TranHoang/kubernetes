4a5ef672d0ee11874c21a9b8d187ab3edda7d741
Tran Hoang <tran.hoangnguyen@asnet.com.vn>
2018-04-05 14:41:28 +0700

    Update vagrant box

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
+# Install docker
+sudo apt-get install docker.io --yes
+
 #Install python-pip
 sudo apt-get install software-properties-common
 sudo apt-add-repository universe
@@ -12,10 +15,13 @@ wget https://github.com/kubernetes/kops/releases/download/1.8.1/kops-linux-amd64
 chmod +x kops-linux-amd64
 sudo mv kops-linux-amd64 /usr/local/bin/kops
 
-#1. Set up Kubectl
+# Set up Kubectl
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 sudo mv kubectl /usr/local/bin/
 sudo chmod +x /usr/local/bin/kubectl
 
 # Load AWS enviroment variable
-set -o allexport; source /kubernetes/env/aws.env; set +
+set -o allexport; source /k8s/env/aws.env; source /k8s/env/cluster.env; set +
+
+# Alow ubuntu user can run docker command
+sudo usermod -a -G docker $USER
