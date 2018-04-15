497ee86d7b269c5ba8b14518e76fc30b31862d59
Tran Hoang <tran.hoangnguyen@asnet.com.vn>
2018-04-15 16:17:43 +0700

    Add scripts to deployment by ReplicationController

---
+++ bin/create-repl-controller.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+APP_NAME="helloworld"
+
+# Get the devops helloworld folder path
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+APP_DEVOPS_FOLDER="$(dirname "$SCRIPTPATH")/devops/$APP_NAME"
+
+kubectl create -f $APP_DEVOPS_FOLDER/repl-controller.yml
